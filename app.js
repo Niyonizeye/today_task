6e9c81f615e623f5e595f7de305682a952e279f6
unknown <niyoeanpaul@gmail.com>
2021-05-20 21:14:03 +0200

    The CORS for enable AcCess

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 
 
@@ -12,6 +19,7 @@ const subtaskRoute = require('./routes/subtask');
 app.use('/todo/v1', todoRoute);
 app.use('/subtask/v1', subtaskRoute);
 
+// const mongo_Url = 'mongodb+srv://JeanPaul:dusabimana2019@cluster0.uwya5.mongodb.net/Todo?retrytrueWrites=true';
 const mongo_Url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.uwya5.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true`;
 // const mongo_Url = `mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false`;
 mongoose.connect(mongo_Url)
