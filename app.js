3ed51e65c57de56e04e7c95d51ef493f6c23f8f8
unknown <niyoeanpaul@gmail.com>
2021-05-20 11:58:21 +0200

    test for production

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const subtaskRoute = require('./routes/subtask');
 app.use('/todo/v1', todoRoute);
 app.use('/subtask/v1', subtaskRoute);
 
-
-const mongo_Url = `mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false`;
+const mongo_Url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.uwya5.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true`;
+// const mongo_Url = `mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false`;
 mongoose.connect(mongo_Url)
 .then( result => {
     app.listen(3000, () => {
