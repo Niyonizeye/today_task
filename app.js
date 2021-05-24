06b7277a216145623fc136f14458b0b50a3dda45
unknown <niyoeanpaul@gmail.com>
2021-05-24 07:48:34 +0200

    let see

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Methods: POST, PUT, GET, OPTIONS);
+  res.header("Access-Control-Allow-Origin", "POST, PUT, GET, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
